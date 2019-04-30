bb3b04b5a19c152a7ae532763331981dd36940d3
LarrySimiyu <larry.simiyu@gmail.com>
2019-04-29 20:10:45 -0700

    initial push

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const helmet = require('helmet');
-
 const server = express();
 
 server.use(express.json());
@@ -12,3 +11,5 @@ const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
 });
+
+//initial push
