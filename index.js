1636d0bc85176f52d8df52a4d0d2ea7377b50500
LarrySimiyu <larry.simiyu@gmail.com>
2019-05-02 11:59:37 -0700

    end points added, server running with no errors

--- index.js
+++ index.js
@@ -2,14 +2,112 @@ const express = require('express');
 const helmet = require('helmet');
 const server = express();
 
+
+
+const router = require('express').Router();
+const knex = require('knex');
+
+const knexConfig = {
+  client: 'sqlite3',
+  useNullAsDefault: true,
+  connection: {
+    filename: './data/lambda.sqlite3'
+  },
+  debug: true,
+};
+const db = knex(knexConfig);
+//teaches knex how to find our dataBase
+
 server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+
+server.get('/', (req, res) => {
+  db('zoos') //returns a promise that resolves to all records in the tables 
+    // then and catch are the bros to get to the actual data 
+    .then(zoos => {
+      res.status(200).json(zoos); // if there were no errors this happens 
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    })
+  res.send('get');
+})
+
+
+// get id 
+
+server.get('/:id', (req, res) => {
+  const { id } = req.params;
+  db('roles')
+    .where({ id })
+    .first()
+    .then(zoo => {
+      res.status(200).json(zoo)
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+// put
+
+server.put('/:id', (req, res) => {
+  db('zoos')
+    .where({ id: req.params.id })
+    .update(req.body)
+    .then(count => {
+      if (count > 0) {
+        res.status(200).json(count)
+      } else {
+        res.status(404).json({ message: 'Animal not found' })
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err)
+    });
+});
+
+
+//delete
+
+server.delete('/:id', (req, res) => {
+  db('zoos')
+    .where({ id: req.params.id })
+    .del()
+    .then(count => {
+      if (count > 0) {
+        res.status(200).end();
+      } else {
+        res.status(404).json({ message: 'Animal not found' })
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err)
+    });
+})
+
 
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
 });
 
-//initial push
+
+
+// server.get('/api/zoos'), (req, res) => {
+//     res.send('get zoos');
+// }
+
+// server.get('/api/zoos/:id'), (req, res) => {
+//     res.send('get zoos');
+// }
+
+// server.delete('/api/zoos/:id'), (req, res) => {
+//     res.send('get zoos');
+// }
+
+// server.put('/api/zoos/:id'), (req, res) => {
+//     res.send('get zoos');
+// }
---
+++ routers/zoo-router.js
@@ -0,0 +1,19 @@
+
+// server.get('/api/zoos'), (req, res) => {
+//     res.send('get zoos');
+// }
+
+// server.get('/api/zoos/:id'), (req, res) => {
+//     res.send('get zoos');
+// }
+
+// server.delete('/api/zoos/:id'), (req, res) => {
+//     res.send('get zoos');
+// }
+
+// server.put('/api/zoos/:id'), (req, res) => {
+//     res.send('get zoos');
+// }
+
+
+module.exports = router; 
